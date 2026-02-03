0ded1f95222751ea0947b5ac31c35d79618fcdc3
Daniel Bautista <dbautista9015@gmail.com>
2026-02-02 22:31:54 -0800

    Add mason plugin

---
+++ lua/config/plugins/mason.lua
@@ -0,0 +1,18 @@
+-- Portable package manager for Neovim that runs everywhere Neovim runs.
+-- Easily install and manage LSP servers, DAP servers, linters, and formatters.
+return {
+  {
+    'mason-org/mason.nvim',
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end
+  }
+}
