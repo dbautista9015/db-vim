ec76caa2da8a9c181c97b62519d224edf1dc4134
Daniel Bautista <dbautista9015@gmail.com>
2026-02-11 22:37:10 -0800

    Change colorscheme to catppuccin

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
   {
-    "navarasu/onedark.nvim",
-    priority = 1000, -- load this before all the other start plugins
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-      require('onedark').setup {
-        style = 'darker'
-      }
-      require('onedark').load()
+      vim.cmd.colorscheme "catppuccin-macchiato"
     end
   }
 }
